08ad3f86aedc531f356dab96ea6b2192ab485912
huyen010 <nkh062001@gmail.com>
2022-06-28 17:43:45 +0700

    updatev5

---
+++ src/main/java/com/finalExam/orderdrinkapp/Activities_clone/ThongtinActivity.java
@@ -0,0 +1,16 @@
+package com.finalExam.orderdrinkapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.finalExam.orderdrinkapp.R;
+
+public class ThongtinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thongtin);
+    }
+}
